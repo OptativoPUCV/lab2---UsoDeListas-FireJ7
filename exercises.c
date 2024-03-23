ee8ea6403aa98a7a6dd109e9f6fb3f8479388672
Your Name <you@example.com>
2024-03-23 22:03:58 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -113,16 +113,17 @@ void copia_pila(Stack* P1, Stack* P2)
   Stack* P3 = create_stack();
   for (int i = 0; i < get_size(P1); i++)
     {
-      char *str = (char*)first(P1);
-      push(P3, str);
-      popFront(P1);
+      int *dato = (int*)first(P1);
+      push(P3,dato);
+      pop(P1);
     }
   for (int i = 0; i < get_size(P3); i++)
     {
-      char *str = (char*)first(P3);
-      push(P2, str);
-      popFront(P3);
+      int *dato = (int*)first(P3);
+      push(P2,dato);
+      pop(P3);
     }
+    
 }
 
 /*
